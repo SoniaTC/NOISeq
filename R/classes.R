af3b55ee41d65f48c1b53b6b7f0f77fb9ca2c70d
Sonia Tarazona <starazona@cipf.es>
2012-11-06 16:13:59 +0000

    Some important changes made
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/NOISeq@70988 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/classes.R
+++ R/classes.R
@@ -12,12 +12,12 @@ setGeneric("explo.plot", function(object, ...) standardGeneric("explo.plot"))
 setMethod("explo.plot", "Biodetection", function(object) biodetection.plot(object@dat))
 setMethod("explo.plot", "CD", function(object) cd.plot(object@dat))
 setMethod("explo.plot", "CountsBio", function(object, toplot = 1, samples = NULL, ylim = NULL)
-          countsbio.plot(object@dat, toplot = 1, samples = NULL, ylim = NULL))
+          countsbio.plot(object@dat, toplot = toplot, samples = samples, ylim = ylim))
 setMethod("explo.plot", "DLBio", function(object, samples = NULL, toplot = "protein_coding", ylim = NULL)
-          DLbio.plot(object@dat, samples = NULL, toplot = "protein_coding",ylim = NULL))
+          DLbio.plot(object@dat, samples = samples, toplot = toplot,ylim = ylim))
 setMethod("explo.plot", "Saturation",
           function(object, toplot = 1, samples = NULL, ylim = NULL, yrightlim = NULL)
-          saturation.plot(object@dat, toplot = 1, samples = NULL, ylim = NULL, yrightlim = NULL))
+          saturation.plot(object@dat, toplot = toplot, samples = samples, ylim = ylim, yrightlim = yrightlim))
 
 # Show methods for exploration objects
 setMethod("show", "Biodetection",
@@ -38,19 +38,33 @@ setMethod("show", "CD",
     })
 
 setMethod("show", "CountsBio",
-    function(object) {
+          function(object) {
+            for (i in c(1:length(object@dat$quart))) {
+              cat("\n",names(object@dat$quart)[i],"\n============\n")
+              aux <- cbind(object@dat$quart[[i]],object@dat$bionum[rownames(object@dat$quart[[i]])])
+              colnames(aux)[4] <- "bionum"
+              print(head(aux))
+            }
+            
+          })
 
-      cat("\nSummary\n=======\n")
-      print(head(cbind(object@dat$quart,object@dat$bionum)))
-      
-    })
+setMethod("show","DLBio",
+          function(object) {
+            x <- dat2save(object)
+            print(head(x[[1]]))
+          })
 
+setMethod("show","Saturation",
+          function(object) {
+            x <- dat2save(object)
+            print(head(x[[1]]))
+          })
 
 # Coercion methods for exploration objects
 
-setGeneric("dat2save", function(object, ...)  standardGeneric("dat2save"))
+setGeneric("dat2save", function(object)  standardGeneric("dat2save"))
 
-setMethod("dat2save","Biodetection", function(object, ...) {
+setMethod("dat2save","Biodetection", function(object) {
   aux <- list()
 
   if (length(object@dat$samples) == 2)
@@ -63,22 +77,85 @@ setMethod("dat2save","Biodetection", function(object, ...) {
   aux  
 })
 
-setMethod("dat2save","CD", function(object, ...) object@dat)
+setMethod("dat2save","CD", function(object) object@dat)
 
-setMethod("dat2save","CountsBio", function(object, ...)  {
+setMethod("dat2save","CountsBio", function(object)  {
 
-  x <- cbind(object@dat$quart, object@dat$bionum[rownames(object@dat$quart)])
-  colnames(x)[4] <- "BioNum"
+  x <- list()
+  for (i in c(1:length(object@dat$quart))) {
+    aux <- cbind(object@dat$quart[[i]],object@dat$bionum[rownames(object@dat$quart[[i]])])
+    colnames(aux)[4] <- "bionum"
+    x[[i]] <- aux
+  }
+
+  names(x) <- names(object@dat$quart)
 
   x
 
   })
 
-#setMethod("dat2save","DLBio", function(object, ...) DLbio.plot(object@dat, ...))
-#setMethod("dat2save","Saturation", function(object, ...) saturation.plot(object@dat, ...))
+setMethod("dat2save","DLBio", function(object) {
 
+  biotipos <- names(object@dat$result)
+  muestras <- names(object@dat$depth)
 
+  mat <- matrix(0, nrow = length(biotipos), ncol = 7, dimnames=list(biotipos, c(1:7)))
+  lista <- list()
 
+  for (i in 1:length(muestras)) {
+    mat.aux <- mat
+
+    for (j in 1:length(biotipos)) {
+      mat.aux[j,3:7] <- object@dat$result[[j]][[i]]      
+    }
+
+    mat.aux[,1] <- object@dat$bionum
+    mat.aux[,2] <- object@dat$biolength
+
+    colnames(mat.aux) <- c("bionum","biolength",
+                           paste("depth_",sprintf("%0.3f", object@dat$depth[[i]]/10^6), sep=""))
+    
+    lista[[i]] <- mat.aux
+  }
+
+  names(lista) <- muestras
+  lista
+  
+})
+
+setMethod("dat2save","Saturation", function(object) {
+
+  biotipos <- names(object@dat$saturation)
+  muestras <- names(object@dat$depth)
+
+  mat <- matrix(0, nrow = length(biotipos), ncol = 11, dimnames=list(biotipos, c(1:11)))
+  lista <- list()
+
+  for ( i in 1:length(muestras)) {
+    mat.aux <- mat
+
+    for (j in 1:length(biotipos)) {
+      mat.aux[j,2:6]  <- object@dat$saturation[[j]][[i]]
+      mat.aux[j,7:11] <- object@dat$newdet[[j]][[i]]
+    }
+
+    mat.aux[,1] <- object@dat$bionum
+
+    colnames(mat.aux) <- c("bionum",
+                           paste("num", sprintf("%0.3f", object@dat$depth[[i]]/10^6),sep="_"),
+                           paste("new", sprintf("%0.3f", object@dat$depth[[i]]/10^6),sep="_"))
+    
+    lista[[i]] <- mat.aux
+  }
+
+  names(lista) <- muestras
+  lista  
+  
+})
+
+############################################################################
+############################# OUTPUT OBJECT ################################
+############################################################################
 
 setClass("myInfo",representation(method="character", k="numeric", lc="numeric", factor="vector",
                   v="numeric",nss="numeric",pnr="numeric",comparison="vector",replicates="character"))
